42e99dbb61c48f73086291781376838986877730
George Saad <gsparqcode@gmail.com>
2022-04-22 23:48:54 -0400

    added basic prompt for user, asking for github username

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+// const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUserQuestions = () => {
+  const questions = [
+    {
+      type: "input",
+      name: "gitHubName",
+      message: "What is your GithubName?",
+    },
+  ];
+  return inquirer.prompt(questions);
+};
+promptUserQuestions().then((answers) => console.log(answers));
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
